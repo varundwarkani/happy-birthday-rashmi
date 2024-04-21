7655a917533b7f6892e4b233b93b739a78801ecc
Varun Manoj <Varun_Manoj@Trimble.com>
2024-04-21 16:11:59 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3767;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
